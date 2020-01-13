10710d5820697dca88643cb65e4b73828c10914d
RafalJimi <56085348+RafalJimi@users.noreply.github.com>
2020-01-13 13:39:07 +0100

    Add files via upload

--- src/modules/posts/components/postTitle.js
+++ src/modules/posts/components/postTitle.js
@@ -4,8 +4,7 @@ export const PostTitle = ({ title, url, domain}) => (
     <h6 className="text-dark cursor-pointer">
         <a href={url} className="text-dark" role="button">
             {title}
-        </a> 
         {domain &&
-        <small className="text-muted"> ({domain})</small>}
+        <small className="text-muted"> ({domain})</small>}</a> 
     </h6>
 );
