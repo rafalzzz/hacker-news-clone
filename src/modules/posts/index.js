db98c2267d088fe9e65ceae7080c7a4611134e0f
RafalJimi <56085348+RafalJimi@users.noreply.github.com>
2020-01-13 13:06:28 +0100

    Add files via upload

--- src/modules/posts/index.js
+++ src/modules/posts/index.js
@@ -1,4 +1,4 @@
-import React, { memo, useCallback, useState } from 'react';
+import React, { memo, useCallback } from 'react';
 import { PostsLayout } from './layout';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPostsStarted } from '../../store/posts/actions';
@@ -6,35 +6,19 @@ import { selectPostById, selectPosts } from '../../store/posts/selectors';
 
 export const Posts = memo(() => {
 
-  const [postsType, setPostsType] = useState('news');
-  const [page, setPage] = useState(1)
-
     const dispatch = useDispatch();
-    const handleFetchPosts = useCallback((type, page) => {
-            dispatch(fetchPostsStarted({ type: type, page: page}));
+    const handleFetchPosts = useCallback((postsType, page) => {
+            dispatch(fetchPostsStarted({ type: postsType, page: page}));
 }, []);
-
-  const handleChangePostsType = e => {
-    setPostsType(e.target.value);
-    setPage(1);
-    handleFetchPosts(postsType, page)
-  }
-
-  const handleLoadNextPage = () => {
-    setPage(page + 1);
-    handleFetchPosts(postsType, page)
-  }
-
 
 const posts = useSelector(selectPosts);
 const postById = useSelector(selectPostById(22002062));
 
 return (
     <PostsLayout
-      handleChangePostsType={handleChangePostsType}
+    handleFetchPosts={handleFetchPosts}
       posts={posts}
       postById={postById}
-      handleLoadNextPage={handleLoadNextPage}
     />
   );
 });
